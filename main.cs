ab368a69a6998e9a802662f750f4031510099db2
ebiak <ebrahim.amiri.k@gmail.com>
2020-11-23 18:46:15 +0330

    version bump

--- main.cs
+++ main.cs
@@ -13,7 +13,7 @@ namespace Impostor.Plugins.ImpostorCord
         package: "com.Tuxinal.ImpostorCord",
         name: "ImpostorCord",
         author: "Tuxinal",
-        version: "0.1.5")]
+        version: "0.1.6")]
     public class ImpostorCord : PluginBase
     {
         private readonly ILogger<ImpostorCord> _logger;
